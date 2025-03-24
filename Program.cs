c08ed4294504e88272f23fc3f3e792fcfb5b00f9
Heusraph <raphaelbautista2@gmail.com>
2025-03-25 01:10:35 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,24 +20,6 @@ namespace DailyPlanner
 
         }
 
-        static string UserName()
-        {
-            Console.WriteLine("Hi!, Welcome to Daily Planner!");
-            Console.Write("Please Enter your name: ");
-            return Console.ReadLine();
-        }
-
-        static string Task(string timeofDay)
-        {
-            Console.WriteLine($"What will you do this {timeofDay}?");
-            string task = Console.ReadLine();
-            Console.WriteLine($"That's a wonderfull thing to do this {timeofDay}!");
-            return task;
-        }
-        static void SayGoodbye(string name)
-        {
-            Console.WriteLine($"Goodnight {name}, sleepwell and see you tomorrow!");
-
 
     }
 }

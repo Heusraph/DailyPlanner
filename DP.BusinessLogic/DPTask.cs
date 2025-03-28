5a597c4bdfd3cc465b898ac18d3b1e4325fe3ed0
Heusraph <raphaelbautista2@gmail.com>
2025-03-29 00:04:43 +0800

    Major Changes

---
+++ DP.BusinessLogic/DPTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP.BusinessDataLogic
+{
+    public class DPTask
+    {
+        public static List<string> tasks = new List<string>();
+        public static List<string> times = new List<string>();
+
+        public static void AddTask(string task, string time)
+        {
+
+
+            DPTask.tasks.Add(task);
+            DPTask.times.Add(time);
+
+
+        }
+    }
+}
